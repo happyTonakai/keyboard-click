f3b566aedec904ee0b6c01af940938f2b3198c7c
happyTonakai <happyangry@foxmail.com>
2024-08-15 13:18:21 +0800

    Add file version. Increase to v1.1

--- make.ps1
+++ make.ps1
@@ -1,6 +1,13 @@
-pyinstaller --windowed --onefile --clean --add-data 'switch-on.png;.' --add-data 'switch-off.png;.' keymouse.py
-If (Test-Path ~/keymouse.exe) {
-	Remove-Item ~/keymouse.exe
-}
-Copy-Item dist/keymouse.exe ~/keymouse.exe
-Start-Process ~/keymouse.exe
+<#
+Author: Zerui Han <hanzr.nju@outlook.com>
+Date: 2023-01-17 12:54:37
+Description: 
+FilePath: /keyboard-click/make.ps1
+LastEditTime: 2024-08-15 13:11:16
+#>
+pyinstaller --windowed --onefile --clean --add-data 'switch-on.png;.' --add-data 'switch-off.png;.' --version-file=file_version_info.txt keymouse.py
+# If (Test-Path ~/keymouse.exe) {
+	# Remove-Item ~/keymouse.exe
+# }
+# Copy-Item dist/keymouse.exe ~/keymouse.exe
+# Start-Process ~/keymouse.exe
